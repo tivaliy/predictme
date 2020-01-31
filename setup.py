9e3f8e9ed6cbe18649ce46652b291fa483b58849
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2020-01-31 16:42:21 +0200

    Install from package from github, with github dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,28 @@ with io.open(os.path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
+def install_deps():
+    """
+    Reads requirements.txt and preprocess it to be feed into setuptools.
+
+    Returns:
+         list of packages and dependency links.
+    """
+    default = open('requirements.txt', 'r').readlines()
+    new_pkgs = []
+    links = []
+    for resource in default:
+        if 'git+ssh' in resource:
+            pkg = resource.split('#')[-1]
+            links.append(resource.strip() + '-9876543210')
+            new_pkgs.append(pkg.replace('egg=', '').rstrip())
+        else:
+            new_pkgs.append(resource.strip())
+    return new_pkgs, links
+
+
+pkgs, new_links = install_deps()
+
 setuptools.setup(
     name='predictme',
     description='ML-based object prediction library',
@@ -20,6 +42,8 @@ setuptools.setup(
     author_email='vitaliy@kulanov.org.ua',
     packages=setuptools.find_packages(where='src'),
     package_dir={'': 'src'},
+    install_requires=pkgs,
+    dependency_links=new_links,
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
